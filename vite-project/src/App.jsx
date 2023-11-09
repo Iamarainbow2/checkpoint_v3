ce1b90de5b03ee7606ad1c3cae4140e56b294c7a
Mert <mmdurmacalis@gmail.com>
2023-11-09 10:55:57 +0300

    a

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
-    </>
+    <BrowserRouter>
+     <Routes>
+       <Route path="/" element={<Home />}/>
+       <Route path="/address/:id" element={<AddressDetails />}/>
+       <Route path="/personal/:id" element={<PersonalDetails />}/>
+     </Routes>
+    </BrowserRouter> 
+   </>
   )
 }
 
